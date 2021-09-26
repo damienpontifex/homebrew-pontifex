6d19ba437398f08831a526e7b277d83730598b5c
Damien Pontifex <damien.pontifex@gmail.com>
2021-09-26 10:07:22 +0800

    Update bump-tap.rb

--- Formula/bump-tap.rb
+++ Formula/bump-tap.rb
@@ -1,14 +1,7 @@
 class BumpTap < Formula
-  desc "Bump tap"
+  desc "A tap sample to test out github actions bumping tap version"
   homepage "https://github.com/damienpontifex/bump-tap"
-  version "0.0.1"
   url "https://github.com/damienpontifex/bump-tap/releases/download/v0.0.1/main.sh"
+  version "0.0.1"
   sha256 "1051032fe1768f4ba3bd2532d8cc0202526e7babdd7385fb5114994f34c051d5"
-
-  def install
-    bin.install "main.sh"
-  end
-
-  test do
-  end
 end
